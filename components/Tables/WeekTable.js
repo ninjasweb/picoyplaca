5a31236dfe1acae98dcfe6e98364b1516cd9ef1a
NinjasWeb <hola@ninjasweb.com>
2023-07-23 02:26:47 -0500

    responsive and others fixes

--- components/Tables/WeekTable.js
+++ components/Tables/WeekTable.js
@@ -45,7 +45,15 @@ const WeekTable = ({ city, lastNumer }) => {
   // Obtener las restricciones para la ciudad actual
   const restrictions = cityRestrictions[city] || {}
 
-  const daysOfWeek = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+  const daysOfWeek = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
+  
+
+  const currentDate = new Date();
+  // Función para obtener la cantidad de días en un mes específico
+  const getDaysInMonth = (year, month) => {
+    return new Date(year, month + 1, 0).getDate();
+  };
+  
 
   return (
     <table className={styles.table}>
@@ -59,9 +67,26 @@ const WeekTable = ({ city, lastNumer }) => {
         {daysOfWeek.map((day, index) => {
           const dayRestrictions = restrictions[index+1] || []
           const isRestricted = dayRestrictions.includes(parseFloat(lastNumer))
+
+          // Calcular la fecha para cada día de la semana
+          const currentDay = currentDate.getDate();
+          const currentMonth = currentDate.getMonth();
+          const dayToAdd = index - currentDate.getDay() + 1; // Sumar días para obtener el día de la semana correspondiente
+          const dateForDay = new Date(currentDate);
+          dateForDay.setDate(currentDay + dayToAdd);
+
+          // Verificar si estamos en el mes siguiente
+          if (dateForDay.getMonth() !== currentMonth) {
+            // Si estamos en el mes siguiente, ajustar la fecha
+            const lastDayOfMonth = getDaysInMonth(dateForDay.getFullYear(), currentMonth);
+            dateForDay.setDate(lastDayOfMonth);
+          }
+
+          const dayNumber = dateForDay.getDate();
+          const monthNumber = dateForDay.getMonth() + 1;
           return(
             <tr className={isRestricted ? styles.restricted : ''} key={index}>
-              <td>{day}</td>
+              <td>{day} {dayNumber}/{monthNumber}</td>
               <td>
               {restrictions[index + 1] && restrictions[index + 1].length > 0
                 ? `Restringido para placas terminadas en ${restrictions[index + 1].join('-')}`
