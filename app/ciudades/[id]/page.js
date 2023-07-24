91cfb7a89bc78da5e49b297b76132e1082b30b31
NinjasWeb <hola@ninjasweb.com>
2023-07-23 22:09:18 -0500

    Fix quote error

--- app/ciudades/[id]/page.js
+++ app/ciudades/[id]/page.js
@@ -34,7 +34,7 @@ const City = ({ params }) => {
         <h1>Pico y placa hoy en {cityData.name}</h1>
         <Badge/>
         <h2>Pico y Placa Hoy en {cityData.name}: Restricciones y Horarios para Carros Particulares</h2>
-        <p style={{marginTop: '1rem'}}>Si estás conduciendo un carro particular en la ciudad de {cityData.name}, es esencial que estés al tanto de las restricciones de tráfico conocidas como 'Pico y Placa'. Estas regulaciones buscan mejorar la movilidad y reducir la congestión vehicular en la ciudad. A continuación, te proporcionamos toda la información que necesitas para evitar contratiempos y cumplir con las normativas vigentes.</p>
+        <p style={{marginTop: '1rem'}}>Si estás conduciendo un carro particular en la ciudad de {cityData.name}, es esencial que estés al tanto de las restricciones de tráfico conocidas como <b>Pico y Placa</b>. Estas regulaciones buscan mejorar la movilidad y reducir la congestión vehicular en la ciudad. A continuación, te proporcionamos toda la información que necesitas para evitar contratiempos y cumplir con las normativas vigentes.</p>
         <h2>Pico y placa para la ciudad de {cityData.name}</h2>
         <p style={{textAlign: 'center'}}>Carros Particulares</p>
         <Times city={cityData.name}/>
