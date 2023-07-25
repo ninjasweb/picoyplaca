8d1efa64f4f866e617880b73b6f7a67da8be572f
NinjasWeb <hola@ninjasweb.com>
2023-07-24 23:13:28 -0500

    Api v.0.1

---
+++ app/api/cities/route.js
@@ -0,0 +1,48 @@
+import { cityRestrictions } from "@/data/carsRestrictions"
+import { cities } from "@/data/cities"
+import { NextRequest, NextResponse } from "next/server"
+
+export async function GET(NextRequest) {
+  const city = NextRequest.nextUrl.searchParams.get('city')
+  
+  if (!city) {
+    // Si no se proporciona el parámetro 'city', devuelve todas las restricciones
+    return NextResponse.json(cityRestrictions, {
+      status: 200,
+    });
+  }
+
+  // Busca la ciudad en el objeto cities por su slug en minúsculas
+  const selectedCity = cities.find((c) => c.slug === city.toLowerCase());
+
+  if (!selectedCity) {
+    // Si la ciudad no se encuentra en el objeto cities, devuelve un mensaje de error
+    return NextResponse.json(
+      { error: "City not found" },
+      {
+        status: 404,
+      }
+    );
+  }
+
+  // Obtiene el nombre de la ciudad con la primera letra en mayúscula (ejemplo: "Ibagué")
+  const cityName = selectedCity.name;
+
+  // Busca las restricciones de la ciudad en el objeto cityRestrictions
+  const cityRestriction = cityRestrictions[cityName];
+
+  if (!cityRestriction) {
+    // Si no se encuentran restricciones para la ciudad, devuelve un mensaje de error
+    return NextResponse.json(
+      { error: "City restrictions not found" },
+      {
+        status: 404,
+      }
+    );
+  }
+
+  // Si se encuentra la ciudad y sus restricciones, devuelve las restricciones para esa ciudad
+  return NextResponse.json(cityRestriction, {
+    status: 200,
+  });
+}
